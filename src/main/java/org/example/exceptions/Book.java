b1421e3c6364bf7893ed4664f06fc581456438e3
Vitaly Gubarev <vitaly1090@mail.ru>
2025-05-07 12:49:27 +0300

    Добавляю решение задачи по модулю исключения

---
+++ src/main/java/org/example/exceptions/Book.java
@@ -0,0 +1,46 @@
+package org.example.exceptions;
+
+public class Book {
+    private String title;
+    private String author;
+    private int availableCopies;
+
+    public Book(String title, String author, int availableCopies) {
+        this.title = title;
+        this.author = author;
+        this.availableCopies = availableCopies;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", availableCopies=" + availableCopies +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/exceptions/NoAvailableCopiesException.java
@@ -0,0 +1,8 @@
+package org.example.exceptions;
+
+public class NoAvailableCopiesException extends Exception {
+
+    public NoAvailableCopiesException(String message) {
+        super(message);
+    }
+}
